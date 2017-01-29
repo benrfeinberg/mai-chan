a411c7728ffdcd5bc238755af743b5bd477f4604
Ben Feinberg <benfeinberg@bfeinbergs-mbp.attlocal.net>
2017-01-28 18:57:31 -0800

    more animals

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ var media = {
   a: {
     image: "http://www.animalstown.com/animals/a/anteater/wallpapers/anteater-wallpaper-5.jpg",
     sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/32cc49bdbf3f4cf4bdbafa83e11c3179_anteater-001.mp3",
-    text: "anteter"
+    text: "anteater"
   },
   b: {
-    image: "https://static.pexels.com/photos/2740/nature-animal-fur-dangerous.jpg",
+    image: "https://static.pexels.com/photos/35435/pexels-photo.jpg",
     sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/65b790212fb846efb133c0ce746801ff_bear-001.mp3",
     text: "bear"
   },
@@ -32,9 +32,9 @@ var media = {
     text: "frog"
   },
   g: {
-    image: "https://static.pexels.com/photos/38534/giraffe-wild-wildlife-nature-38534.jpeg",
-    sound: "",
-    text: "giraffe"
+    image: "https://static.pexels.com/photos/35992/gorilla-monkey-ape-zoo.jpg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/b5d75ab5069646d7a68ca90a43e2279b_gorilla-001.mp3",
+    text: "gorilla"
   },
   h: {
     image: "https://static.pexels.com/photos/208799/pexels-photo-208799.jpeg",
@@ -42,9 +42,9 @@ var media = {
     text: "horse"
   },
   i: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/167804/pexels-photo-167804.jpeg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/2fbc94686ceb434a864c0691906bd144_dog.mp3",
+    text: "inu"
   },
   j: {
     image: "https://static.pexels.com/photos/37535/leopard-cat-savuti-botswana-37535.jpeg",
@@ -52,9 +52,9 @@ var media = {
     text: "jaguar"
   },
   k: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/57848/pexels-photo-57848.jpeg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/65b790212fb846efb133c0ce746801ff_bear-001.mp3",
+    text: "kuma"
   },
   l: {
     image: "https://static.pexels.com/photos/59863/sheep-white-lambs-goats-59863.jpeg",
@@ -62,24 +62,24 @@ var media = {
     text: "Lamb"
   },
   m: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/97827/pexels-photo-97827.jpeg",
+    sound: "http://www.animal-sounds.org/jungle/Monkey%20chatter%20animals059.wav",
+    text: "monkey"
   },
   n: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/171227/pexels-photo-171227.jpeg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/ddf67da01ed84f67aacc127ca42871b5_domestic-cat-001.mp3",
+    text: "neko"
   },
   o: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/6646/nature-animal-fur-wolf.jpeg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/dcdafbcae2a64b3aa0ea59526aa75f02_wolf-001.mp3",
+    text: "ookami"
   },
   p: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/185368/pexels-photo-185368.jpeg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/11b0b4456ef94e88951e39db3d16a1cd_penguin.mp3",
+    text: "penguin"
   },
   q: {
     image: "",
@@ -93,18 +93,18 @@ var media = {
   },
   s: {
     image: "https://static.pexels.com/photos/18103/pexels-photo.jpg",
-    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/a05f8c4ef4264978bc0644cad176cca6_harbor-seal-pup-001.mp3",
-    text: "seal"
+    sound: "",
+    text: "saru"
   },
   t: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/54602/raccoon-bear-zoo-saeugentier-54602.jpeg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/9a319b8f1ce24adfa5e03384ff0404d2_racoon-babies-001.mp3",
+    text: "tanuki"
   },
   u: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/208799/pexels-photo-208799.jpeg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/052ec56730374386b5f471a8e67647c8_clydesdale.mp3",
+    text: "uma"
   },
   v: {
     image: "",
@@ -127,9 +127,9 @@ var media = {
     text: ""
   },
   z: {
-    image: "",
-    sound: "",
-    text: ""
+    image: "https://static.pexels.com/photos/31594/pexels-photo.jpg",
+    sound: "http://c0026106.cdn1.cloudfiles.rackspacecloud.com/71c17a1c80ff45f495df479edb0665c5_elephant-001.mp3",
+    text: "zo"
   }
 }
 
